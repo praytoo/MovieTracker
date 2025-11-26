b91298a08b31ed15f65baaa36308e2b14e71be27
Arisu <oshosh12909@gmail.com>
2025-11-26 09:28:38 -0800

    Saving working manual version before new pull

--- src/main/java/org/movietracker/dao/WatchListDAO.java
+++ src/main/java/org/movietracker/dao/WatchListDAO.java
@@ -2,8 +2,6 @@ package org.movietracker.dao;
 
 import org.movietracker.model.Movie;
 import org.movietracker.model.WatchList;
-// We import * so we get Genre and ParentalRating too
-import org.movietracker.model.*;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -12,22 +10,23 @@ import java.util.stream.Collectors;
 
 public class WatchListDAO {
 
-    // 1. Get all movies in a user's watchlist
+    // 1. Get WatchList
     public List<WatchList> getWatchList(int userId) {
         List<WatchList> userWatchList = new ArrayList<>();
-        String sql = "SELECT * FROM Wish_List WHERE user_id = ?";
 
-        // Connect to database and run the command
+        String sql = "SELECT w.id as wishlist_id, w.user_id, m.* " +
+                "FROM Wish_List_Relation w " +
+                "JOIN Movies m ON w.movie_id = m.movie_id " +
+                "WHERE w.user_id = ?";
+
         try (Connection conn = DatabaseManager.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setInt(1, userId);
             ResultSet rs = stmt.executeQuery();
 
-            // Go through the results one by one
             while (rs.next()) {
                 WatchList movie = new WatchList();
-                // Copy info from database to our java object
                 movie.setWishlistId(rs.getInt("wishlist_id"));
                 movie.setUserId(rs.getInt("user_id"));
                 movie.setMovieId(rs.getInt("movie_id"));
@@ -36,7 +35,6 @@ public class WatchListDAO {
                 movie.setParentalRating(rs.getString("parental_rating"));
                 movie.setDescription(rs.getString("description"));
 
-                // Add it to our list
                 userWatchList.add(movie);
             }
         } catch (SQLException e) {
@@ -45,12 +43,9 @@ public class WatchListDAO {
         return userWatchList;
     }
 
-    // 2. Add a movie to user's watchlist
+    // 2. Add to WatchList
     public void addToWatchList(int userId, int movieId) {
-        // Copy the movie info from Movies table to Wish_List table
-        String sql = "INSERT INTO Wish_List (user_id, movie_id, title, avg_percentage_rating, parental_rating, description) " +
-                "SELECT ?, movie_id, title, avg_percentage_rating, parental_rating, description " +
-                "FROM Movies WHERE movie_id = ?";
+        String sql = "INSERT INTO Wish_List_Relation (user_id, movie_id) VALUES (?, ?)";
 
         try (Connection conn = DatabaseManager.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
@@ -59,11 +54,10 @@ public class WatchListDAO {
             stmt.setInt(2, movieId);
 
             int rowsAffected = stmt.executeUpdate();
-
             if (rowsAffected > 0) {
                 System.out.println("Success! Movie " + movieId + " added to watchlist.");
             } else {
-                System.out.println("Error: Could not find movie with ID " + movieId);
+                System.out.println("Could not add movie. Check ID.");
             }
 
         } catch (SQLException e) {
@@ -71,29 +65,28 @@ public class WatchListDAO {
         }
     }
 
-    // 3. Rate a movie (Updates the real Movies table)
+    // 3. Rate a Movie
     public void postMovieRating(int userId, int movieId, int rating) {
-        String sql = "UPDATE Movies SET individual_star_rating = ? WHERE movie_id = ?";
+        String sql = "INSERT INTO Ratings (user_id, movie_id, rating_value) VALUES (?, ?, ?)";
 
         try (Connection conn = DatabaseManager.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-            stmt.setInt(1, rating);
+            stmt.setInt(1, userId);
             stmt.setInt(2, movieId);
+            stmt.setInt(3, rating);
 
             int rowsAffected = stmt.executeUpdate();
-
             if (rowsAffected > 0) {
-                System.out.println("Success! You rated movie " + movieId);
-            } else {
-                System.out.println("Error: Movie not found.");
+                System.out.println("Success! Rated movie " + movieId);
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    // 4. Get top 5 movies (Required: Use Java Streams)
+    // 4. Get Top 5 Movies
     public List<Movie> getTop5Movies() {
         List<Movie> allMovies = new ArrayList<>();
         String sql = "SELECT * FROM Movies";
@@ -102,16 +95,18 @@ public class WatchListDAO {
              PreparedStatement stmt = conn.prepareStatement(sql);
              ResultSet rs = stmt.executeQuery()) {
 
-            // Get every single movie from database first
             while (rs.next()) {
-                // Since the Lead used Lombok, we use .builder() to make the object
-                // Note: We skip Genre/Actors for now to keep it simple and safe
-                Movie movie = Movie.builder()
-                        .movie_id(rs.getDouble("movie_id"))
-                        .title(rs.getString("title"))
-                        .avrRating(rs.getDouble("avg_percentage_rating"))
-                        .description(rs.getString("description"))
-                        .build();
+                // MANUAL WAY (Replaced Builder with standard setters)
+                Movie movie = new Movie();
+                movie.setMovie_id(rs.getDouble("movie_id"));
+                movie.setTitle(rs.getString("title"));
+                movie.setAvrRating(rs.getDouble("avg_percentage_rating"));
+                movie.setDescription(rs.getString("description"));
+
+                // Leaving complex fields null for now to prevent errors
+                movie.setGenre(null);
+                movie.setCast(null);
+                movie.setParentalRating(null);
 
                 allMovies.add(movie);
             }
@@ -120,11 +115,6 @@ public class WatchListDAO {
             e.printStackTrace();
         }
 
-        // NOW we use Streams (This is the requirement!)
-        // 1. Stream the list
-        // 2. Sort by rating (high to low)
-        // 3. Keep only top 5
-        // 4. Turn back into a List
         return allMovies.stream()
                 .sorted(Comparator.comparingDouble(Movie::getAvrRating).reversed())
                 .limit(5)
--- src/main/java/org/movietracker/model/Movie.java
+++ src/main/java/org/movietracker/model/Movie.java
@@ -1,15 +1,8 @@
 package org.movietracker.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-
 import java.util.Date;
 import java.util.List;
 
-@Data
-@Builder
-@AllArgsConstructor
 public class Movie {
     private double movie_id;
     private String title;
@@ -19,4 +12,45 @@ public class Movie {
     private double avrRating;
     private String description;
     private Date releasedDate;
+
+    // Empty Constructor
+    public Movie() {
+    }
+
+    // Full Constructor
+    public Movie(double movie_id, String title, Genre genre, List<Actor> cast, ParentalRating parentalRating, double avrRating, String description, Date releasedDate) {
+        this.movie_id = movie_id;
+        this.title = title;
+        this.genre = genre;
+        this.cast = cast;
+        this.parentalRating = parentalRating;
+        this.avrRating = avrRating;
+        this.description = description;
+        this.releasedDate = releasedDate;
+    }
+
+    // Getters and Setters (Manual)
+    public double getMovie_id() { return movie_id; }
+    public void setMovie_id(double movie_id) { this.movie_id = movie_id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public Genre getGenre() { return genre; }
+    public void setGenre(Genre genre) { this.genre = genre; }
+
+    public List<Actor> getCast() { return cast; }
+    public void setCast(List<Actor> cast) { this.cast = cast; }
+
+    public ParentalRating getParentalRating() { return parentalRating; }
+    public void setParentalRating(ParentalRating parentalRating) { this.parentalRating = parentalRating; }
+
+    public double getAvrRating() { return avrRating; }
+    public void setAvrRating(double avrRating) { this.avrRating = avrRating; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public Date getReleasedDate() { return releasedDate; }
+    public void setReleasedDate(Date releasedDate) { this.releasedDate = releasedDate; }
 }
